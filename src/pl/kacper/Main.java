f2a81204484f7d0b204cbaadbef076fddde1d9a5
Kacper <kacper_s11@wp.pl>
2021-02-15 17:56:06 +0100

    Initial commit

---
+++ src/pl/kacper/Main.java
@@ -0,0 +1,9 @@
+package pl.kacper;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("aha");
+    }
+}
---
+++ src/pl/kacper/TestClass.java
@@ -0,0 +1,13 @@
+package pl.kacper;
+
+public class TestClass {
+    public int age;
+    String name;
+
+    public TestClass() {
+    }
+
+    public TestClass(int age1, String name1) {
+        age = age1;
+    }
+}
